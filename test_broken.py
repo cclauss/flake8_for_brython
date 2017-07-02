30246800f8ba10e90cd804eddb657559736d43c1
cclauss <cclauss@bluewin.ch>
2017-07-02 19:07:58 +0200

    Update test_broken.py

--- test_broken.py
+++ test_broken.py
@@ -7,26 +7,48 @@ from database import Database
 # Database unfortunately makes db.__MAX_ID private :-(
 MAX_ID = 493
 
+tuple_store = False
 db = Database()
+try:
+    db.get_kanto
+except AttributeError:
+    tuple_store = True
+    
 print('From https://en.wikipedia.org/wiki/Pok%C3%A9mon#Generation_1 ...')
 counts = {'kanto': 151, 'johto': 100, 'hoenn': 135, 'sinnoh': 107, 'all': 493}
 
 
 def test_kanto_length():  # passes
-    assert len(db.get_kanto()) == 151  # Passes
+    if tuple_store:
+        assert len(db.get_region('kanto') == 151  # Passes
+    else:
+        assert len(db.get_kanto()) == 151  # Passes
+        
 
 
 def test_johto_length():  # fails:
-    assert len(db.get_johto()) == 100  # Fails: assert 101 == 100
+    if tuple_store:
+        assert len(db.get_region('johto') == 100  # Passes
+    else:
+        assert len(db.get_johto()) == 100  # Fails: assert 101 == 100
 
 
 def test_hoenn_length():  # fails:
-    assert len(db.get_hoenn()) == 135  # Fails: assert 144 == 135
+    if tuple_store:
+        assert len(db.get_region('hoenn') == 135  # Passes
+    else:
+        assert len(db.get_hoenn()) == 135  # Fails: assert 144 == 135
 
 
 def test_sinnoh_length():  # fails:
-    assert len(db.get_sinnoh()) == 107  # Fails: assert 121 == 107
+    if tuple_store:
+        assert len(db.get_region('sinnoh') == 107  # Passes
+    else:
+        assert len(db.get_sinnoh()) == 107  # Fails: assert 121 == 107
 
 
 def test_extra_length():  # fails: 0 record
-    assert db.get_extra()  # Fails: returns zero pokemon!'
+    if tuple_store:
+        assert db.get_region('extra')  # Passes
+    else:
+        assert db.get_extra()  # Fails: returns zero pokemon!'
