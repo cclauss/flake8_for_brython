9527dcc259d10f63b91f89325f730bbb5d0032f7
cclauss <cclauss@bluewin.ch>
2017-07-02 17:30:17 +0200

    Update test_broken.py

--- test_broken.py
+++ test_broken.py
@@ -7,45 +7,26 @@ from database import Database
 # Database unfortunately makes db.__MAX_ID private :-(
 MAX_ID = 493
 
+db = Database()
 print('From https://en.wikipedia.org/wiki/Pok%C3%A9mon#Generation_1 ...')
 counts = {'kanto': 151, 'johto': 100, 'hoenn': 135, 'sinnoh': 107, 'all': 493}
 
 
 def test_kanto_length():  # passes
-    # expected = counts['kanto']
-    # actual = len(Database().get_kanto())
-    # msg = 'kanto expected: {}\n  actual: {}'.format(expected, actual)
-    # print(msg)
-    # assert expected == actual, msg
-    assert len(Database().get_kanto()) == 151 == counts['kanto']
+    assert len(db.get_kanto()) == 151  # Passes
 
 
 def test_johto_length():  # fails:
-    # expected = counts['johto']
-    # actual = len(Database().get_johto())
-    # msg = 'johto expected: {}\n  actual: {}'.format(expected, actual)
-    # print(msg)
-    # assert expected == actual, msg
-    assert len(Database().get_johto()) == 100 == counts['johto']
+    assert len(db.get_johto()) == 100  # Fails: assert 101 == 100
 
 
 def test_hoenn_length():  # fails:
-    # expected = counts['hoenn']
-    # actual = len(Database().get_hoenn())
-    # msg = 'hoenn expected: {}\n  actual: {}'.format(expected, actual)
-    # print(msg)
-    # assert expected == actual, msg
-    assert len(Database().get_hoenn()) == 135 == counts['hoenn']
+    assert len(db.get_hoenn()) == 135  # Fails: assert 144 == 135
 
 
 def test_sinnoh_length():  # fails:
-    # expected = counts['sinnoh']
-    # actual = len(Database().get_sinnoh())
-    # msg = 'sinnoh expected: {}\n  actual: {}'.format(expected, actual)
-    # print(msg)
-    # assert expected == actual, msg
-    assert len(Database().get_sinnoh()) == 107 == counts['sinnoh']
+    assert len(db.get_sinnoh()) == 107  # Fails: assert 121 == 107
 
 
 def test_extra_length():  # fails: 0 record
-    assert Database().get_extra(), 'db.get_extra() returns no pokemon!'
+    assert db.get_extra()  # Fails: returns zero pokemon!'
