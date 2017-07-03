0e3f0c8f9e52af21ac63ff7fa0bf6f688b0de71f
cclauss <cclauss@bluewin.ch>
2017-07-03 12:47:00 +0200

    Update test_broken.py

--- test_broken.py
+++ test_broken.py
@@ -3,7 +3,9 @@
 # To run the tests, use: python3 -m pytest --capture=sys
 
 from database import Database
+from test_database import make_extra_counts
 
+extra_counts = make_extra_counts()
 tuple_store = False
 try:
     Database.MAX_ID  # Old Database makes db.__MAX_ID private
@@ -13,43 +15,49 @@ except AttributeError:
 
 print('From https://en.wikipedia.org/wiki/Pok%C3%A9mon#Generation_1 ...')
 counts = {'kanto': 151, 'johto': 100, 'hoenn': 135, 'sinnoh': 107, 'all': 493}
-
-
-def test_kanto_length():  # passes
-    db = Database()
-    if tuple_store:
-        assert len(db.get_region('kanto')) == 151  # Passes
-    else:
-        assert len(db.get_kanto()) == 151  # Passes
-
-
-def test_johto_length():  # fails:
-    db = Database()
-    if tuple_store:
-        assert len(db.get_region('johto')) == 100  # Passes
-    else:
-        assert len(db.get_johto()) == 100  # Fails: assert 101 == 100
-
-
-def test_hoenn_length():  # fails:
-    db = Database()
-    if tuple_store:
-        assert len(db.get_region('hoenn')) == 135  # Passes
-    else:
-        assert len(db.get_hoenn()) == 135  # Fails: assert 144 == 135
-
-
-def test_sinnoh_length():  # fails:
-    db = Database()
-    if tuple_store:
-        assert len(db.get_region('sinnoh')) == 107  # Passes
-    else:
-        assert len(db.get_sinnoh()) == 107  # Fails: assert 121 == 107
+for region_name, extra_count in extra_counts.items():
+    counts[region_name] += extra_count  # add the extras to the wikipedia counts
 
 
 def test_extra_length():  # fails: 0 record
-    db = Database()
-    if tuple_store:
-        assert db.get_region('extra')  # Passes
-    else:
-        assert db.get_extra()  # Fails: returns zero pokemon!'
+    # db = Database()
+    # if tuple_store:
+    #     assert db.get_region('extra')  # Passes
+    # else:
+    assert Database().get_extra()  # Fails: returns zero pokemon!'
+
+
+def test_kanto_length():  # passes
+    region_name = 'kanto'
+    # db = Database()
+    # if tuple_store:
+    #    assert len(db.get_region(region_name)) == expected_len  # Fails
+    # else:
+    assert len(db.get_kanto()) == counts[region_name]  # Passes
+
+
+def test_johto_length():  # fails:
+    region_name = 'johto'
+    # db = Database()
+    # if tuple_store:
+    #    assert len(db.get_region(region_name)) == expected_len  # Fails
+    # else:
+    assert len(db.get_johto()) == counts[region_name]  # Passes
+
+
+def test_hoenn_length():  # fails:
+    region_name = 'hoenn'
+    # db = Database()
+    # if tuple_store:
+    #    assert len(db.get_region(region_name)) == expected_len  # Fails
+    # else:
+    assert len(db.get_hoenn()) == counts[region_name]  # Passes
+
+
+def test_sinnoh_length():  # fails:
+    region_name = 'sinnoh'
+    # db = Database()
+    #if tuple_store:
+    #    assert len(db.get_region(region_name)) == expected_len  # Fails
+    # else:
+    assert len(db.get_sinnoh()) == counts[region_name]  # Passes
